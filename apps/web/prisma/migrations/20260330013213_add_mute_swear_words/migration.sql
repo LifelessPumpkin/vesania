ff23aace3e9d74b5e72bf2ae762fbc78162e58e1
Logan Harmon <l3harmon@hotmail.com>
2026-04-29 15:22:04 -0400

    Fixed build errors and reset the db completely

--- apps/web/prisma/migrations/20260330013213_add_mute_swear_words/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "muteSwearWords" BOOLEAN NOT NULL DEFAULT false;
--- apps/web/prisma/migrations/20260401201245_drop_gold/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `gold` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "gold";
